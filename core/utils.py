b4908c300ac5d0b1c7b80940f27c8d43fd3ef34d
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-05-12 01:17:34 +0800

    Add util function

---
+++ core/utils.py
@@ -0,0 +1,9 @@
+
+
+def get_object_or_none(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        return None
+    except model.MultipleObjectsReturned:
+        return None

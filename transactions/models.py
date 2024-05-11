59ef6b3daea45e141d187570a84efc11669f7713
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-05-11 22:42:22 +0800

    Add transaction table and add parent to category table

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class TransactionTypes(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    transaction_type = models.ForeignKey(
+        TransactionTypes, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             related_name='transactions')
+    category = models.ForeignKey('contenttypes.ContentType',
+                                 on_delete=models.CASCADE)
+    account = models.ForeignKey('contenttypes.ContentType',
+                                on_delete=models.CASCADE,
+                                related_name='transactions')
+
+    def __str__(self):
+        return f'{self.transaction_type.name} - {self.amount} on {self.date}'

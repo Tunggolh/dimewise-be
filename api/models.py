5cdbb711fb4cba3744716f30df751e799980dab0
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-05-12 01:23:52 +0800

    Remove core app

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from categories.models import Category
+from transactions.models import TransactionType
 
-# Create your models here.
+
+class TransactionTypeCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    transaction_type = models.ForeignKey(
+        TransactionType, on_delete=models.CASCADE, related_name='categories')
+
+    class Meta:
+        unique_together = ('category', 'transaction_type')
+
+    def __str__(self):
+        return f'{self.transaction_type.name} - {self.category.name}'

b04f15ff60c6d9f0a905ddd11e01256efbbe4c4e
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-05-18 20:30:01 +0800

    Fix user namespace

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ from django.urls import path
 
 from users import views
 
-app_name = 'user'
+app_name = 'users'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),

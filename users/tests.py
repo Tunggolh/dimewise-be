c727b4ef7853d1acad955644ef27de05eb0a987e
Marlo Tunggolh <marlotunggolh@gmail.com>
2024-05-18 18:45:08 +0800

    Move unit test for user

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+"""
+Tests for models
+"""
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
